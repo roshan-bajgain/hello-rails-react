ea8a7be7b19919852c268199d5a5631d51367512
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-07-12 09:27:42 +0545

    Solve the problem

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -2,6 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 class HelloWorld extends React.Component {
   render () {
+    console.log('rendering react component');
     return (
       <React.Fragment>
         Greeting: {this.props.greeting}

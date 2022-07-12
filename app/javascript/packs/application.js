c63c742c12e68492cc91522942e10cff0885864f
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-07-12 10:23:09 +0545

    fix error

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,7 @@
 // const imagePath = (name) => images(name, true)
 
 console.log('Hello World from Webpacker')
+
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,18 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "static#index"
+  # root "articles#index"
 
-  namespace :v1, defaults: { formate: 'json' } do
-    get 'things', to: 'things#index'
+  namespace :v1, default: { format: 'json'} do
+    get 'greetings' => 'greetings#index'
   end
+
+  # react routes
+
+  get '*page', to: 'static#index', constraints: ->(req) do
+  !req.xhr? && req.format.html?
+end
+
+  #root
+  root 'static#index'
 end
